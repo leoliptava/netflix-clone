6bf89d017e953cebfb4ec6f8803c3a5845c79ac5
Leonardo Felipe <tleonardo186@gmail.com>
2021-09-05 13:27:39 -0300

    Alterando imagens do header

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,13 +7,17 @@ export default function Header(props) {
         <header className={props.black ? styles.black : ''}>
             <div className={styles.headerTopo}>
                 <a href="#">
-                    <Image src={'/../public/logo-netflix.png'} alt={'Logo'} width={100} height={25}/>
+                    {/* <Image src={'/../public/logo-netflix.png'} alt={'Logo'} width={100} height={25}/> */}
+                    {/* eslint-disable-next-line @next/next/no-img-element*/}
+                    <img src="logo-netflix.png" alt="Logo Netflix" />
                 </a>
             </div>
 
             <div className={styles.headerUser}>
                 <a href="#">
-                    <Image src={'/../public/avatar-netflix.png'} alt={'Avatar'} width={50} height={50}/>
+                    {/* <Image src={'/../public/avatar-netflix.png'} alt={'Avatar'} width={50} height={50}/> */}
+                    {/* eslint-disable-next-line @next/next/no-img-element*/}
+                    <img src="avatar-netflix.png" alt="User" />
                 </a>
             </div>
         </header>
